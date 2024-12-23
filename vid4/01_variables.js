df7388a3a8500f47ce753fbd55d10e051bf611c1
Atharva Dhumal <atharvaadhumal@gmail.com>
2024-12-24 01:16:22 +0530

    let const

---
+++ vid4/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 121212
+let accountEmail = "atharva@gmail.com"
+var accountPass = "atharva"
+accountCity = "Thane"
+
+// accountId = 2
+accountEmail = "juhi@gmail.com"
+accountPass = "atharvadhumal"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity])
+
