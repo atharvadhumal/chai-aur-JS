21bfc96136ca7aa9b1d21ca0aa0da9fb542669e0
Atharva Dhumal <atharvaadhumal@gmail.com>
2024-12-24 17:24:44 +0530

    arrays part2 learned

--- arrays/main.js
+++ arrays/main.js
@@ -1,8 +1,26 @@
-const myArr = [0, 1, 2, 3, 4, 5, 6]
-console.log(myArr.includes(4));
+// Arrays part 2
 
-// myArr.push(7)
-// myArr.push(8)
-// myArr.pop()
-// console.log(myArr)
 
+// const marvelHeros = ["spiderman", "thor" , "hulk"]
+// const dcHeros = ["joker", "batman" , "superman"]
+
+// marvelHeros.push(dcHeros);
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+// const allHeros = [...marvelHeros, ...dcHeros] //using spread  
+// console.log(allHeros);
+
+// const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const realArray = anotherArray.flat(Infinity) //Infinity or we can give a number like 3 4 for depth in which array is gng to be sovled
+// console.log(realArray);
+
+console.log(Array.isArray("Atharva")) //will give boolean is this is a array or not
+console.log(Array.from("Hitesh")) //will make this an array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); //will make a array of these values
