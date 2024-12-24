6ee5f09671e11dbf8c4c6a45870ac67774d06f0d
Atharva Dhumal <atharvaadhumal@gmail.com>
2024-12-24 12:55:39 +0530

    learned about strings

---
+++ 03_conversionsoperations/main.js
@@ -0,0 +1,22 @@
+// const name = "atharva"
+// const repoCount = 4
+
+// // console.log(name + repoCount + " value");   this way is good but we have a cleaner way
+
+// console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+// const gameName = "Atharva"
+
+// console.log(gameName[0]);
+// console.log(gameName.length);  //gives the length of the string
+ 
+// console.log(gameName.toUpperCase()); // makes the string to uppercase
+
+// const newStringOne = "   Atharva   "
+// console.log(newStringOne); // will print with spaces
+// console.log(newStringOne.trim()); // will print without spaces as trim will remove unnecessary spaces
+
+// const url = "https://atharva.com/atharva%20dhumal"
+
+// console.log(url.replace('%20', '-' )); // will replace %20 with -
+`       `
