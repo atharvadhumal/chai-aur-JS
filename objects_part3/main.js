0b248c1f5722caa6372fd04ac96d4c6fee22c3ad
Atharva Dhumal <atharvaadhumal@gmail.com>
2024-12-24 23:56:31 +0530

    learned about de-structuring

---
+++ objects_part3/main.js
@@ -0,0 +1,7 @@
+const course = {
+    courseName: "JS in Hindi",
+    Price: 999,
+    courseTeacher: "hitesh",
+}
+const {courseTeacher: coach} = course //we have named the courseTeacher as coach here it will print the same value
+console.log(coach)
