550ef7f94b7d57f0d6837217c47e2756aa8ac077
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-01-06 13:06:23 +0530

    learned about while loop

---
+++ whileloop/main.js
@@ -0,0 +1,21 @@
+// let index = 0
+// while ( index <= 10) {
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2 // we increamented index by 2 if we have done index++ it would have imcreament by 1
+// }
+
+// let myArray = ["flash", "superman", "batman"]
+// let arr = 0
+//  while ( arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+//  }
+
+
+let  score = 1
+ 
+do{ //means do this while score is less than equal to 10 
+    console.log(`score is ${score}`)
+    score++ //score increase by 1 or else loop will be stuck at same place
+}
+while ( score <= 10); 
